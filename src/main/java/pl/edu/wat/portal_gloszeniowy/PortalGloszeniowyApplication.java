c27cce8f299aec73ca11c5e37aa514050ef9e84e
Adam Majbour-Majek <adammajbour@gmail.com>
2021-06-07 19:48:21 +0200

    Start

---
+++ src/main/java/pl/edu/wat/portal_gloszeniowy/PortalGloszeniowyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.portal_gloszeniowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalGloszeniowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalGloszeniowyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/wat/portal_gloszeniowy/PortalGloszeniowyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.portal_gloszeniowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalGloszeniowyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
